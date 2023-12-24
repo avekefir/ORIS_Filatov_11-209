19d3047147b33c594105a1232eac13f2fcbf01a1
Ilya Filatov <avekefir@mail.ru>
2023-12-24 23:22:30 +0300

    [add] Homework_3

---
+++ Homework_3/HttpServer/HttpServer/Config/serverConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+
+namespace HttpServer
+{
+    public static class ServerConfiguration
+    {
+        public static Config _config { get; }
+
+        static ServerConfiguration()
+        {
+            try
+            {
+                using (var file = File.OpenRead(@"appsettings.json"))
+                {
+                    _config = JsonSerializer.Deserialize<Config>(file) ?? throw new Exception();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Config file not found: " + @"appsettings.json");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading server configuration from " + @"appsettings.json");
+                throw;
+            }
+
+        }
+    }
+
+    public class Config
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public string StaticFilesPath { get; set; }
+    }
+}
+
